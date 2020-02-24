3cae68bd841ed233ec9290198ec013fe3113c25f
Maxim Berge <maxim.berge@gmail.com>
2020-02-24 23:37:35 +0100

    add variable for number of card to crap in screen.py

--- screen.py
+++ screen.py
@@ -1,8 +1,10 @@
+"""File to scrap randomly generated credit cards."""
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+NUMBER_OF_CARD_TO_SCRAP = 2000
 
 driver = webdriver.Firefox()
-for i in range(1000):
+for i in range(NUMBER_OF_CARD_TO_SCRAP):
     driver.get('https://graphtylove.github.io/card-generator/')
     driver.save_screenshot("cards_data/screen-" + str(i) + ".png")
